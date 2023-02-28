5e95b3dd193cbc81e018e85d6fbaed8a6dfc3724
Adam Hess <adamhess89@gmail.com>
2023-02-28 10:19:37 -0800

    added footer and refactored

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ var navbar =
                     '<a class="nav-link" href="/">Home</a>'+
                 '</li>'+
                 '<li class="nav-item dropdown">'+
-                    '<a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Seasons</a>'+
-                    '<ul class="dropdown-menu">'+
+                    '<a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" data-bs-theme="dark" aria-expanded="false">Seasons</a>'+
+                    '<ul class="dropdown-menu" data-bs-theme="dark">'+
                     '<li><a class="dropdown-item" href="season44.html">Season 44</a></li>'+
                     '</ul>'+
                 '</li>'+
@@ -22,3 +22,15 @@ var navbar =
 
 var nav_insert = document.getElementById('nav');
 nav_insert.insertAdjacentHTML('beforeend', navbar);
+
+var footer = 
+'<div class="container-fluid text-left text-light bg-dark bg-gradient">'+
+'<div class="row">'+
+  '<div class="col py-1">'+
+    '<h5>Adam Hess 2023</h5>'+
+  '</div>'+
+'</div>'+
+'</div>'
+
+var footer_insert = document.getElementById('foot');
+footer_insert.insertAdjacentHTML('beforeend', footer);
